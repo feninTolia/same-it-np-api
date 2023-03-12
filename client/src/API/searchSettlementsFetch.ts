efd027f1a4fe971b20868e6893ea8538606ff1c4
feninTolia <fenintolja@gmail.com>
2023-03-12 17:47:09 +0200

    added dotenv

--- client/src/API/searchSettlementsFetch.ts
+++ client/src/API/searchSettlementsFetch.ts
@@ -1,8 +1,6 @@
-const API_KEY = '47bebc438b872c9c970902470e6eaba0';
-
 export const searchSettlementsFetch = async (CityName: string) => {
   const requestBody = {
-    apiKey: API_KEY,
+    apiKey: import.meta.env.NP_API_KEY,
     modelName: 'Address',
     calledMethod: 'searchSettlements',
     methodProperties: {
